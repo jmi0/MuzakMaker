34903062759c26d34296b9c40367d0486a3b23c9
Joseph <joseph.m.iannone@gmail.com>
2017-09-14 20:16:56 -0400

    chordBlocks

---
+++ chordBlock.py
@@ -0,0 +1,18 @@
+
+class ChordBlock(object):
+
+        def __init__(chord, beats, style):
+                self.chord = chord
+                self.beats = beats
+                self.style = style
+                self.block = []
+                self.__buildBlock()
+
+        def getBlock(self):
+                #TODO
+                pass
+
+        def __buildBlock(self):
+                # TODO
+                pass
+
