d51117c1bb253f62ee01d29f29a758b989900d4c
Joseph <joseph.m.iannone@gmail.com>
2017-09-24 16:52:46 -0400

    chord blocks

--- chordBlock.py
+++ chordBlock.py
@@ -2,11 +2,17 @@ import random
 
 class ChordBlock(object):
 
-        def __init__(self, chord, dur, style):
-                self.chord = chord
-		self.dur = dur
+        def __init__(self, chord, style, timesig):
+                if timesig == 4:
+			self.beats = [0, 1, 2, 3]
+		elif timesig == 3:
+			self.beats = [0, 1, 2]
+		self.time = [0,1,2,3]
+		self.timesig = timesig
+		self.chord = chord
                 self.style = style
                 self.block = []
+		self.time = []
                 self.__buildBlock()
 
         def getBlock(self):
@@ -14,6 +20,6 @@ class ChordBlock(object):
                 pass
 
         def __buildBlock(self):
-                # TODO
-                pass
+                for i in range(0, len(chord) - 1):
+			self.time.append(random.choice())
 
