5ba7d427fc6336afacd6fe68c9d001bec1491e52
Joseph <joseph.m.iannone@gmail.com>
2017-09-26 19:02:14 -0400

    ending/melodies

---
+++ melody.py
@@ -0,0 +1,15 @@
+import random
+
+class Melody(object):
+
+        def __init__(self):
+		#TODO
+                pass
+
+        def getBlock(self):
+                #TODO
+                pass
+
+        def __buildBlock(self):
+        	#TODO
+		pass
--- song.py
+++ song.py
@@ -1,4 +1,6 @@
 import random
+import datetime
+import time
 from createScale import stayInKey
 from structure import Structure
 from chordBlock import ChordBlock
@@ -12,7 +14,7 @@ class Song(object):
 		self.timesig = timesig
                 self.measures = measures
 		self.sections = sections
-		self.beats = self.timesig * self.measures
+		self.beats = (self.measures * self.timesig) / self.timesig
                 self.genre = genre
 		self.midi = midi
 		
@@ -25,7 +27,15 @@ class Song(object):
 	
 	def __getRandomDur(self):
 		return random.randint(1, 12)
-	
+
+	def __createAndSave(self):
+		ts = time.time()
+		dt = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S')
+		with open(self.title + "_" + dt + ".mid", "wb") as output_file:
+    			self.midi.writeFile(output_file)
+
+
+
 	def create(self):
 		self.midi.addTempo(0, 0, self.tempo)
 		
@@ -37,9 +47,10 @@ class Song(object):
 	
 		secs = struct.getSections()
 		currentbeat = 1
-
+	
 		for i in range(0, len(secs)):
-        		print secs[i]
+        		
+			print secs[i]
                 	chords = secs[i].getSection()[0]
                 	chordDurs = secs[i].getSection()[1]
 			#print chords[j].getVoices()
@@ -48,22 +59,45 @@ class Song(object):
 			for j in range(0, len(chords)):
                         	print 'chord voices:'
 				print chords[j].getVoices()
+			
                 		for k in range(0, len(chords[j].getVoices())):
 					#print chords[j].getVoices()[k]
-					self.midi.addNote(0, 0, chords[j].getVoices()[k], currentbeat, chordDurs[j], self.__getRandomVol())
+					print "current beat: "
+					print currentbeat
+					print "total: "
+					print self.beats
+					if self.beats - currentbeat <= 1:
+						dur = self.timesig*2
+					else:
+						dur = chordDurs[j]
+
+					self.midi.addNote(
+						0, 
+						0, 
+						chords[j].getVoices()[k], 
+						currentbeat, 
+						dur, 
+						self.__getRandomVol()
+					)
+				
 				
 				# add bass notes
 				if float(currentbeat).is_integer():
-					self.midi.addNote(0, 0, self.key[self.key.index(random.choice(chords[j].getVoices())) - 7], currentbeat, chordDurs[j], self.__getRandomVol())
+					self.midi.addNote(
+						0, 
+						0, 
+						self.key[self.key.index(random.choice(chords[j].getVoices())) - 7], 
+						currentbeat, 
+						chordDurs[j], 
+						self.__getRandomVol()
+					)
+				
 				currentbeat += chordDurs[j]
 
 			print '\n'
+		
+		self.__createAndSave()
 
-		with open(self.title + ".mid", "wb") as output_file:
-    			self.midi.writeFile(output_file)
-
-
-
-
+	
 
 
